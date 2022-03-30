95fa9c91156e0dfcc1cafd1626119454fdae6a14
Lahiru Hashan <hashanlahiru6@gmail.com>
2022-03-31 02:58:42 +0530

    Update

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -13,9 +13,8 @@ public class ArithmeticOperationsTest {
 
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
-		Integer expected = 8;
+		Integer expected = 800000;
 // 		assertEquals(expected, actual);
-		fail("must fail");
 
 	}
 //     @Test
